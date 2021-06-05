d5cb9d4b147a214b699f990a8343f5a9221e2cd8
vesuviusfin <84998602+vesuviusfin@users.noreply.github.com>
2021-06-05 23:34:37 +0200

    Update index.esm.js

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1864,8 +1864,8 @@ var Icon$P = function (props) {
 
 var Logo = function (_a) {
     var isDark = _a.isDark, props = __rest(_a, ["isDark"]);
-    return (React.createElement(Svg, __assign({ viewBox: "0 0 205 26" }, props),
-        React.createElement("image", { width: "205", height: "26", href: isDark ? '/images/vesuvius/LogoTextNewDark.png' : '/images/vesuvius/LogoTextNewWhite.png' })));
+    return (React.createElement(Svg, __assign({ viewBox: "0 0 205 46" }, props),
+        React.createElement("image", { width: "205", height: "46", href: isDark ? '/images/vesuvius/LogoTextNewDark.png' : '/images/vesuvius/LogoTextNewWhite.png' })));
 };
 
 var Icon$Q = function (props) {
